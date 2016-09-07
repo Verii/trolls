c123f4724345c0d8110013474a30487511c9d501
James Smith <james@apertum.org>
2016-09-07 12:11:42 -0500

    location_distance returns double

--- src/location.c
+++ src/location.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 #include "game.h"
 
-uint32_t
+double
 location_distance(struct location loc1, struct location loc2) {
 	int32_t diff_x, diff_y;
-	diff_x = abs(((int32_t) loc2.x - (int32_t) loc1.x));
-	diff_y = abs(((int32_t) loc2.y - (int32_t) loc1.y));
+	diff_x = ((int32_t) loc2.x - (int32_t) loc1.x);
+	diff_y = ((int32_t) loc2.y - (int32_t) loc1.y);
 
-	double dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2));
-	return (dist < 0) ? (uint32_t) -dist : (uint32_t) dist;
+	return sqrt(pow(diff_x, 2) + pow(diff_y, 2));
+}
 }
