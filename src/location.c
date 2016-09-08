a20dcbc403f79f0fa4e4791a1381d9ea31929e20
James Smith <james@apertum.org>
2016-09-08 07:34:22 -0500

    Clang-format src/location.c
    Also switch some local variables to const

--- src/location.c
+++ src/location.c
@@ -1,29 +1,32 @@
-#include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #include "game.h"
 
 double
-location_distance(struct location loc1, struct location loc2) {
-	int32_t diff_x, diff_y;
-	diff_x = ((int32_t) loc2.x - (int32_t) loc1.x);
-	diff_y = ((int32_t) loc2.y - (int32_t) loc1.y);
+location_distance(struct location loc1, struct location loc2)
+{
+  const int32_t diff_x = ((int32_t)loc2.x - (int32_t)loc1.x);
+  const int32_t diff_y = ((int32_t)loc2.y - (int32_t)loc1.y);
 
-	return sqrt(pow(diff_x, 2) + pow(diff_y, 2));
+  return sqrt(pow(diff_x, 2) + pow(diff_y, 2));
 }
 
 bool
-location_adjacent(struct location loc1, struct location loc2) {
-	if (loc1.x == loc2.x &&
-		  abs((int32_t) loc1.y - (int32_t) loc2.y) == 1)
-		return true;
+location_adjacent(struct location loc1, struct location loc2)
+{
+  const int32_t diff_x = ((int32_t)loc2.x - (int32_t)loc1.x);
+  const int32_t diff_y = ((int32_t)loc2.y - (int32_t)loc1.y);
 
-	if (loc1.y == loc2.y &&
-		  abs((int32_t) loc1.x - (int32_t) loc2.x) == 1)
-		return true;
+  if (loc1.x == loc2.x && abs(diff_y) == 1)
+    return true;
 
-	return false;
+  if (loc1.y == loc2.y && abs(diff_x) == 1)
+    return true;
+
+  return false;
 }
 
 // Returns true if loc2 is adjacent to loc1,
@@ -31,36 +34,35 @@ location_adjacent(struct location loc1, struct location loc2) {
 //
 // If the locations are not adjacent, return false
 bool
-location_relative(struct location loc1, struct location loc2, enum direction *dir) {
-	if (loc1.x == loc2.x) {
-		switch (loc1.y - loc2.y) {
-			case -1:
-				*dir = SOUTH;
-				return true;
+location_relative(struct location loc1, struct location loc2,
+                  enum direction* dir)
+{
+  const int32_t diff_x = ((int32_t)loc2.x - (int32_t)loc1.x);
+  const int32_t diff_y = ((int32_t)loc2.y - (int32_t)loc1.y);
 
-			case 1:
-				*dir = NORTH;
-				return true;
+  if (loc1.x == loc2.x) {
+    switch (diff_y) {
+      case -1:
+        *dir = NORTH;
+        return true;
 
-			default:
-				return false;
-		}
-	}
+      case 1:
+        *dir = SOUTH;
+        return true;
+    }
+  }
 
-	if (loc1.y == loc2.y) {
-		switch (loc1.x - loc2.x) {
-			case -1:
-				*dir = EAST;
-				return true;
+  if (loc1.y == loc2.y) {
+    switch (diff_x) {
+      case -1:
+        *dir = WEST;
+        return true;
 
-			case 1:
-				*dir = WEST;
-				break;
+      case 1:
+        *dir = EAST;
+        return true;
+    }
+  }
 
-			default:
-				return false;
-		}
-	}
-
-	return false;
+  return false;
 }
